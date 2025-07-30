53ce6ecaebe93314a4a53079ca506a1d9b0cc993
Eruxe <szeles.mael@gmail.com>
2025-07-30 23:27:17 +0200

    feat: add movement by step logic

---
+++ scripts/managers/EnemyManager.gd
@@ -0,0 +1,19 @@
+extends Node
+
+
+# Preload the Enemy class so it is available in the current scope
+const  = preload("res://scenes/game/enemy.gd")
+
+func _ready() -> void:
+	EventBus.connect("game_started", _on_game_started)
+	print("EnemyManager initialized")
+	
+	
+	
+func _on_game_started():
+	var enemy_scene = load("res://scenes/game/Enemy.tscn")
+	if enemy_scene:
+		var instance_enemy = enemy_scene.instantiate() as Enemy
+		if instance_enemy is Enemy:
+			get_tree().current_scene.add_child(instance_enemy)
+			instance_enemy.initialize(Vector2(4, 3), 3, 2) # Example values
