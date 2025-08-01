8f943102d52d2f8a304cdd31b433cce53c8be14f
Eruxe <szeles.mael@gmail.com>
2025-08-01 23:14:05 +0200

    fix: use eight beat for continous movement, new game loop

--- scripts/entities/Head.gd
+++ scripts/entities/Head.gd
@@ -3,12 +3,13 @@ extends Area2D
 
 var previous_cell:Vector2 = Vector2.ZERO
 var next_cell:Vector2 = Vector2.ZERO
-var next_cell_2:Vector2 = Vector2.ZERO
 var collided:bool = false
+var current_input:String
 
 func _ready():
 	EventBus.connect("rollback_head", on_rollback_head)
-	EventBus.connect("movement_input_on_beat", trigger_next_move)
+	EventBus.connect("input_buffer_action", _trigger_next_move)
+	EventBus.connect("eight_beat_triggered", _on_movement)
 	self.connect("area_entered", _on_head_collide)
 	self.connect("body_entered", _on_head_collide)
 
@@ -24,39 +25,30 @@ func _physics_process(delta):
 			next_cell
 		)
 		previous_cell = next_cell
-		await get_tree().create_timer(0.1).timeout
-		
-		if not collided:
-			position = TileMapManager.cell_to_position(next_cell_2)
-			EventBus.emit_signal(
-				"head_cell_changed",
-				next_cell,
-				next_cell_2
-			)
-			previous_cell = next_cell_2
+
+func init_head():
+	current_input = "none"
 
 func init_spawn_cell(cell: Vector2):
 	position = TileMapManager.cell_to_position(cell)
 	previous_cell = cell
 	next_cell = cell
-	next_cell_2 = cell
 
-func trigger_next_move(action: String):
-	collided = false
+func _trigger_next_move(action: String):
+	if action in InputManager.movement_inputs:
+		current_input = action
+
+func _on_movement():
 	var input_dict = {
 		"ui_up": TileSet.CELL_NEIGHBOR_TOP_SIDE,
 		"ui_down": TileSet.CELL_NEIGHBOR_BOTTOM_SIDE,
 		"ui_left": TileSet.CELL_NEIGHBOR_LEFT_SIDE,
 		"ui_right": TileSet.CELL_NEIGHBOR_RIGHT_SIDE
 	}
-	if action in input_dict.keys():
+	if current_input in input_dict.keys():
 		next_cell = TileMapManager.get_neighbor_cell(
 			previous_cell,
-			input_dict.get(action)
-		)
-		next_cell_2 = TileMapManager.get_neighbor_cell(
-			next_cell,
-			input_dict.get(action)
+			input_dict.get(current_input)
 		)
 
 func _on_head_collide(object):
@@ -68,6 +60,7 @@ func _on_head_collide(object):
 
 func on_rollback_head(new_cell: Vector2):
 	EventBus.emit_signal("head_rollbacked")
+	current_input = "none"
 	position = TileMapManager.cell_to_position(new_cell)
 	previous_cell = new_cell
 	next_cell = new_cell
