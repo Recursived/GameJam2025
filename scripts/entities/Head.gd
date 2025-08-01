7a3c5c6dd1d327e957dd6b1d0e827e9954f38995
Eruxe <szeles.mael@gmail.com>
2025-08-01 21:28:04 +0200

    fix: made the game easier and faster

--- scripts/entities/Head.gd
+++ scripts/entities/Head.gd
@@ -3,6 +3,8 @@ extends Area2D
 
 var previous_cell:Vector2 = Vector2.ZERO
 var next_cell:Vector2 = Vector2.ZERO
+var next_cell_2:Vector2 = Vector2.ZERO
+var collided:bool = false
 
 func _ready():
 	EventBus.connect("rollback_head", on_rollback_head)
@@ -22,13 +24,25 @@ func _physics_process(delta):
 			next_cell
 		)
 		previous_cell = next_cell
+		await get_tree().create_timer(0.1).timeout
+		
+		if not collided:
+			position = TileMapManager.cell_to_position(next_cell_2)
+			EventBus.emit_signal(
+				"head_cell_changed",
+				next_cell,
+				next_cell_2
+			)
+			previous_cell = next_cell_2
 
 func init_spawn_cell(cell: Vector2):
 	position = TileMapManager.cell_to_position(cell)
 	previous_cell = cell
 	next_cell = cell
+	next_cell_2 = cell
 
 func trigger_next_move(action: String):
+	collided = false
 	var input_dict = {
 		"ui_up": TileSet.CELL_NEIGHBOR_TOP_SIDE,
 		"ui_down": TileSet.CELL_NEIGHBOR_BOTTOM_SIDE,
@@ -40,11 +54,16 @@ func trigger_next_move(action: String):
 			previous_cell,
 			input_dict.get(action)
 		)
+		next_cell_2 = TileMapManager.get_neighbor_cell(
+			next_cell,
+			input_dict.get(action)
+		)
 
 func _on_head_collide(object):
 	if is_instance_of(object, Tail):
 		EventBus.emit_signal("head_on_tail_collision", object)
 	else:
+		collided = true
 		EventBus.emit_signal("head_on_wall_collision")
 
 func on_rollback_head(new_cell: Vector2):
--- scripts/managers/EnemyManager.gd
+++ scripts/managers/EnemyManager.gd
@@ -11,7 +11,15 @@ func _ready() -> void:
 func _on_game_started():
 	var enemy_scene = load("res://scenes/game/Enemy.tscn")
 	if enemy_scene:
-		var instance_enemy = enemy_scene.instantiate() as Enemy
-		if instance_enemy is Enemy:
-			get_tree().current_scene.add_child(instance_enemy)
-			instance_enemy.initialize(Vector2(6, 3), 3, 2) # Example values
+		add_enemy(enemy_scene, 6, 12, 1, 1)
+		add_enemy(enemy_scene, 6, 3, 1, 1)
+		add_enemy(enemy_scene, 8, 5, 1, 1)
+		add_enemy(enemy_scene, 20, 10, 1, 1)
+		add_enemy(enemy_scene, 20, 7, 1, 1)
+		add_enemy(enemy_scene, 25, 8, 1, 1)
+
+func add_enemy(enemy_scene, x, y, width, height):
+	var instance_enemy = enemy_scene.instantiate() as Enemy
+	if instance_enemy is Enemy:
+		get_tree().current_scene.add_child(instance_enemy)
+		instance_enemy.initialize(Vector2(x, y), width, height)
